efbbe42d8ba27bfb61b67dad705c3938be409c18
Rey Garcia <reynaldogarcia@Reynaldos-iMac.local>
2016-09-15 18:25:26 -0600

    u/i

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,126 @@
+var TodoList = function() {
+    this.items = [];
+    this.itemList = $('#item-list');
+    this.itemListTemplate = Handlebars.compile($("#item-list-template").html());
+    this.input = $('#item-input');
+    this.input.keydown(this.onAddInputKeydown.bind(this));
+    this.main = $('#main');
+    this.main.on('dblclick', 'li',
+                 this.onEditItemClicked.bind(this));
+    this.main.on('keydown', 'li input',
+                 this.onEditInputKeydown.bind(this));
+    this.main.on('focusout', 'li input',
+                 this.onEditFocusOut.bind(this));
+    this.main.on('click', 'li .delete-item',
+                 this.onDeleteItemClicked.bind(this));
+    this.getItems();
+};
+
+TodoList.prototype.onAddInputKeydown = function(event){
+	if (event.which != 13) {
+		return;
+	}
+	var input = $(event.target);
+	var value = input.val().trim();
+	if (value!== '') {
+		this.addItem(value);
+	}
+	input.val('');
+	event.preventDefault();
+   };
+TodoList.prototype.onEditItemClicked = function (event) {
+	var item = $(event.target).parents('li');
+	var display = item.children('.display');
+	var input = item.children('input');
+	var name = display.children('.name');
+	input.show();
+	input.focus();
+	input.val(name.text());
+	display.hide();
+	event.preventDefault();
+};
+TodoList.prototype.onEditInputKeydown = function (event) {
+	if(event.which != 13){
+		return;
+		}
+	var input = $(event.target);
+	input.blur();
+};
+TodoList.prototype.onEditFocusOut = function (event) {
+	var item = $(event.target).parents('li');
+	var id = item.data('id');
+	var display = item.children('.display');
+	var input = item.children('input');
+	var name = display.children('.name');
+	var value = input.val().trim();
+	if (value != ''){
+		this.editItem(id, value);
+		name.text(value);
+	}
+	input.hide();
+	display.show();
+	event.preventDefault();
+}
+TodoList.prototype.onDeleteItemClicked = function (event){
+	var id = $(event.target).parents('li').data('id');
+    this.deleteItem(id);
+}
+TodoList.prototype.getItems = function () {
+	var ajax = $.ajax('/items', {
+		type: 'Get',
+		dataType: 'json'
+	});
+	ajax.done(this.onGetItemsDone.bind(this));
+};
+ TodoList.prototype.addItem = function (name){
+ 	var item = {'name':name} ;
+ 	var ajax = $.ajax('/items', {
+ 		type:'POST',
+ 		data: JSON.stringify(item),
+ 		dataType:'json',
+ 		//what does content type and application/json mean
+ 		contentType:'application/json'
+ 	});
+ 	ajax.done(this.getItems.bind(this));
+ };
+
+ TodoList.prototype.deleteItem = function (id) {
+ 	var ajax = $.ajax('/items/' + id, {
+        type: 'DELETE',
+        dataType: 'json'
+    });
+    ajax.done(this.getItems.bind(this));
+};
+ TodoList.prototype.editItem = function (id,name) {
+ 	var item = {'name':name, 'id':id};
+ 	var ajax = $.ajax('/items/' + id, {
+ 		type:'PUT',
+ 		data:JSON.stringify(item),
+ 		dataType:'json',
+ 		 contentType: 'application/json'
+ 	});
+ 	ajax.done(this.getItems.bind(this));
+ };
+TodoList.prototype.onGetItemsDone = function(items) {
+    this.items = items;
+    this.updateItemsView();
+};
+TodoList.prototype.updateItemsView = function() {
+    var context = {
+        items: this.items
+    };
+    var itemList = $(this.itemListTemplate(context));
+    this.itemList.replaceWith(itemList);
+    this.itemList = itemList;
+};
+
 $(document).ready(function() {
     $('.header').hide();
     $('.calendar-wrap').hide();
+    $('.register').hide();
     $('.register').delay(5000).fadeIn();
     $('.header').delay(5000).fadeIn();
     $('.calendar-wrap').delay(7000).fadeIn();
-
+     var app = new TodoList();
 });
+
--- server.js
+++ server.js
@@ -1,14 +1,81 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 //after mongod needed the b
-mongoose.connect('mongodb://localhost/passport-rey');
+mongoose.connect('mongodb://localhost/calendar_app');
+
+app.use(jsonParser);
 
 require('./config/middleware.express')(app);
 //needed app at end of line 9 to work correctly.
 require('./config/routes.express')(app);
 //routes
 
+var Storage = function() {
+    this.items = [];
+    this.id = 0;
+};
+
+Storage.prototype.add = function(name) {
+    var item = {name: name, id: this.id};
+    this.items.push(item);
+    this.id += 1;
+    return item;
+};
+
+var storage = new Storage();
+
+storage.add('fuckl');
+storage.add('shits');
+console.log(storage);
+
+//app will use public directory as static
+app.use(express.static('public'));
+//app get
+app.get('/items/', function(req, res) {
+    res.json(storage.items);
+});
+
+//app post is to add to items
+app.post('/items', jsonParser, function (req, res) {
+
+    if (req.body) {
+        var item = storage.add(req.body.name);
+        res.status(201).json(item);
+    } else {
+        res.sendStatus(400);
+    }
+});
+
+//to edit items 
+app.put('/items/:id', function (req, res) {
+    var id = parseInt(req.params.id);
+    var name = req.body.name;
+    for (var i = 0; i < storage.items.length; i++) {
+        if (storage.items[i].id === id) {
+            storage.items[i].name = name;
+            res.status(201).json(storage.items);
+        }
+    }
+});
+
+//to delete items.
+app.delete('/items/:id', function (req, res) {
+    var id = parseInt(req.params.id);
+    for (var i = 0; i < storage.items.length; i++) {
+        if (storage.items[i].id === id) {
+            storage.items.splice(i, 1);
+            res.status(201).json(storage.items);
+        }
+    }
+}); 
+
 app.listen(8080, function (){
 	console.log('listenting on port 8080');
 });
+
+
+exports.app = app;
+exports.storage = storage;
